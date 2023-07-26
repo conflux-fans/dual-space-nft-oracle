b6d404a3040fc71e55f191d70be3ef577f496515
darwintree <daerwendeshu@outlook.com>
2023-07-26 15:38:07 +0800

    update deployment

--- scripts/authorize_mint_permission.py
+++ scripts/authorize_mint_permission.py
@@ -5,21 +5,21 @@ from conflux_web3.dev import get_testnet_web3
 
 def main():
     load_dotenv()
-    ORACLE_KEY = os.environ["ORACLE_KEY"]
+    AUTHORIZER_KEY = os.environ["AUTHORIZER_KEY"]
     ORACLE_BATCH_NBR = int(os.environ["ORACLE_BATCH_NBR"])
     CORE_CONTRACT_ADDRESS = os.environ["CORE_CONTRACT_ADDRESS"]
     with open("./scripts/DualSpaceNFTCore.json") as f:
         metadata = json.load(f)
 
     w3 = get_testnet_web3()
-    w3.cfx.default_account = w3.account.from_key(ORACLE_KEY)
+    w3.cfx.default_account = w3.account.from_key(AUTHORIZER_KEY)
 
     contract = w3.cfx.contract(address=CORE_CONTRACT_ADDRESS, abi=metadata["abi"])
     # this will not be applied in official env
     contract.functions.batchAuthorizeMintPermission(
         ORACLE_BATCH_NBR,
-        ["crowdin-darwintree"],
-        [4],
+        ["crowdin-darwintree", "github-darwintree"],
+        [4, 5],
     ).transact().executed()
 
 
